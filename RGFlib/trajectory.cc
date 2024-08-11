498497efe4343aa42320a8d3d3c6a6918f6995c2
ninedayhx <1170535490@qq.com>
2024-08-11 20:29:10 +0800

    feat:增加cmake，增加trajectory相关文件

--- RGFlib/trajectory.cc
+++ RGFlib/trajectory.cc
@@ -1,3 +1,13 @@
+/**
+ * @file trajectory.cc
+ * @author ninedayhx (1170535490@qq.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-08-11
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 #include "trajectory.h"
 
 namespace RGF
@@ -8,9 +18,7 @@ namespace RGF
         oldptr = 0;
         nums = n;
         size = 0;
-        points = new trajpoint_t[nums + 1];
-        for (int i = 0; i < nums + 1; i++)
-            points[i] = trajpoint_t();
+        points = new trajpoint_t[nums];
     }
 
     trajectory::~trajectory()
@@ -20,36 +28,65 @@ namespace RGF
 
     void trajectory::addPoint(trajpoint_t pt)
     {
-        ++curptr;
-        if (curptr > nums)
+        static int flag = 0;
+        points[curptr] = pt;
+        if (++curptr > nums)
         {
-            curptr = 1;
-            if (oldptr == 0)
+            curptr = 0;
+            if (!flag)
             {
-                ++oldptr;
+                oldptr = 0;
+                flag = 1;
             }
         }
-        if (oldptr > nums)
+        if (flag)
         {
-            oldptr = 1;
-        }
-        if (oldptr == curptr)
-        {
-            ++oldptr;
-        }
-        points[curptr] = pt;
-        int tmp = curptr - oldptr;
-        if (tmp < 0)
+            oldptr = (curptr + 1) > nums ? 0 : curptr + 1;
             size = nums;
+        }
         else
-            size = tmp;
+        {
+            size = curptr;
+        }
     }
-    trajpoint_t trajectory::findPoint(uint64_t time)
+
+    trajpoint_t *trajectory::findPoint(uint64_t time)
     {
-        return
+        int i = 0;
+        if (time >= points[curptr].timestamp || time <= points[oldptr].timestamp)
+        {
+            return nullptr;
+        }
+        while (findByindex(++i)->timestamp < time)
+        {
+            if (i >= nums)
+                break;
+        }
+        int left = i, right = i + 1;
+
+        return;
+    }
+
+    /**
+     * @brief
+     *
+     * @param idx
+     * @return trajpoint_t*
+     */
+    trajpoint_t *trajectory::findByindex(int idx)
+    {
+        if (idx >= nums)
+            return nullptr;
+        int ptr = curptr - idx;
+        ptr = (ptr >= 0) ? ptr : ptr + nums;
+        return &points[ptr];
     }
 
     void trajectory::updataTrajectory()
+    {
+    }
+
+    trajpoint_t trajectory::interp(trajpoint_t p1, trajpoint_t p2, uint64_t x)
     {
     }
 
--- RGFlib/trajectory.h
+++ RGFlib/trajectory.h
@@ -1,26 +1,51 @@
+/**
+ * @file trajectory.h
+ * @author ninedayhx (1170535490@qq.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-08-11
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 #pragma once
 #include <iostream>
 #include <stdlib.h>
 
-struct trajpoint_t
-{
-    float dpx;
-    float dpy;
-    float heading;
-    uint64_t timestamp;
-    trajpoint_t()
-    {
-        dpx = 0.0f;
-        dpy = 0.0f;
-        heading = 0.0f;
-        timestamp = 0;
-    }
-    trajpoint_t(float x1, float x2, float x3, uint64_t x4)
-        : dpx(x1), dpy(x2), heading(x3), timestamp(x4) {}
-};
-
 namespace RGF
 {
+    /**
+     * @brief
+     *
+     */
+    struct trajpoint_t
+    {
+        float dpx;
+        float dpy;
+        float heading;
+        uint64_t timestamp;
+        trajpoint_t()
+        {
+            dpx = 0.0f;
+            dpy = 0.0f;
+            heading = 0.0f;
+            timestamp = 0;
+        }
+        trajpoint_t(float x1, float x2, float x3, uint64_t x4)
+            : dpx(x1), dpy(x2), heading(x3), timestamp(x4) {}
+        void print()
+        {
+            std::cout << "dpx: " << dpx
+                      << " dpy: " << dpy
+                      << " heading: " << heading
+                      << " t: " << timestamp << std::endl;
+        }
+    };
+
+    /**
+     * @brief
+     *
+     */
     class trajectory
     {
     private:
@@ -30,13 +55,38 @@ namespace RGF
         int size;
         trajpoint_t *points;
 
+    private:
+        /**
+         * @brief
+         *
+         */
+        void updataTrajectory();
+
     public:
         trajectory(int n);
         ~trajectory();
 
+        /**
+         * @brief
+         *
+         * @param pt
+         */
         void addPoint(trajpoint_t pt);
-        trajpoint_t findPoint(uint64_t time);
-        void updataTrajectory();
+        /**
+         * @brief
+         *
+         * @param time
+         * @return trajpoint_t
+         */
+        trajpoint_t *findPoint(uint64_t time);
+        /**
+         * @brief
+         *
+         * @param idx
+         * @return trajpoint_t*
+         */
+        trajpoint_t *findByindex(int idx);
+        trajpoint_t interp(trajpoint_t p1, trajpoint_t p2, uint64_t x);
     };
 
 } // namespace RGF
